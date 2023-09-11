c2ea4b8c3f9496c186ff69e890447b0950ec136e
soyeon <sysy0313@gmail.com>
2023-09-11 23:28:12 +0900

    0911 home save

--- script/index.js
+++ script/index.js
@@ -1,12 +1,14 @@
 // 슬라이드
 const main_top_slide = new Swiper('#main_top_slide',{
     autoplay:{
-        delay:2000,
+        delay:0,
         disableOnInteraction:false,//버튼 클릭 후 자동재생유지
     },
+    speed: 5000,
     loop:true, 
     slidesPerView:'5',
     spaceBetween:'20',
+    loopAdditionalSlides: 1,
 });
 const menu_slide = new Swiper('#menu_slide',{
     autoplay:{
@@ -17,3 +19,12 @@ const menu_slide = new Swiper('#menu_slide',{
     slidesPerView:'4',
     spaceBetween:'20',
 });
+const header = document.querySelector('header')
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset == 0){
+        header.style.background = 'none'
+    }else{
+        header.style.background = '#fff'
+    }
+})
+
